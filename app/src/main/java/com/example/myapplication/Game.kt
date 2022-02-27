e2bbe8415e9816f5418621f2ebfa5e2e83590c1c
Janith Chanaka Abhayarathna <abhayaratnajanith3@gmail.com>
2022-02-27 14:40:49 +0530

    Fixed a bug.

--- app/src/main/java/com/example/myapplication/Game.kt
+++ app/src/main/java/com/example/myapplication/Game.kt
@@ -8,8 +8,8 @@ import java.util.*
 
 class Game : AppCompatActivity() {
 
-    val gen = Random()
-    var expression1 = " "
+    var expression1 = "Expression 01"
+    var expression2 = "Expression 02"
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -23,20 +23,24 @@ class Game : AppCompatActivity() {
         val eqlBtn = findViewById<Button>(R.id.equal)
         val lesBtn = findViewById<Button>(R.id.less)
 
-        var firstNumber = 1 + gen.nextInt(20)
-        var numOfTerms = 1 + gen.nextInt(4)
+        var firstNumber1 = 1 + Random().nextInt(20)
+        var firstNumber2 = 1 + Random().nextInt(20)
 
-        generateExpression1(firstNumber.toString(), numOfTerms, expression01)
+        var numOfTerms1 = 1 + Random().nextInt(4)
+        var numOfTerms2 = 1 + Random().nextInt(4)
+
+        generateExpression1(firstNumber1.toString(), numOfTerms1, expression01)
+        generateExpression2(firstNumber2.toString(), numOfTerms2, expression02)
 
     }
 
-    fun generateExpression1(firstNumber: String, numOfTerms: Int, expression01: TextView) {
+    fun generateExpression1(firstNumber: String, numOfTerms1: Int, expression01: TextView) {
 
-        var numOfTerms = numOfTerms
+        var numOfTerms = numOfTerms1
         val operators: List<String> = listOf("+", "-", "*", "/")
-        val operatorIndex = gen.nextInt(4)
+        val operatorIndex = Random().nextInt(4)
 
-        val secondNumber = 1+ gen.nextInt(20)
+        val secondNumber = 1+ Random().nextInt(20)
 
         expression1 = firstNumber + operators[operatorIndex] + secondNumber
         numOfTerms--
@@ -45,6 +49,24 @@ class Game : AppCompatActivity() {
             generateExpression1("("+expression1+")",numOfTerms, expression01)
         } else {
             expression01.text = expression1
+        }
+    }
+
+    fun generateExpression2(firstNumber2: String, numOfTerms2: Int, expression02: TextView) {
+
+        var numOfTerms = numOfTerms2
+        val operators: List<String> = listOf("+", "-", "*", "/")
+        val operatorIndex = Random().nextInt(4)
+
+        val secondNumber = 1+ Random().nextInt(20)
+
+        expression2 = firstNumber2 + operators[operatorIndex] + secondNumber
+        numOfTerms--
+
+        if (numOfTerms > 0) {
+            generateExpression2("("+expression2+")",numOfTerms, expression02)
+        } else {
+            expression02.text = expression2
         }
     }
 }
